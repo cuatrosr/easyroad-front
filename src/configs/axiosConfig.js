fa49ec7444f8712d7b02ec8a545106cbd66f1c26
David Montaño Tamayo <cuatro04sr@gmail.com>
2024-02-17 12:55:41 -0500

    fix: axios target to docker container

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
 
 export const baseURL = 'http://localhost:3500';
-export const axiosI = axios.create({ baseURL: baseURL });
+export const axiosI = axios.create({
+  baseURL: baseURL,
+  proxy: { host: 'nestjs', port: 3500 },
+});
