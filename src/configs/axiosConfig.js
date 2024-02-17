0fc5fb2b2394d2ba6ae74693ff513e1a27038b91
David Montaño Tamayo <cuatro04sr@gmail.com>
2024-02-17 13:09:18 -0500

    fix: add dns

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const baseURL = 'http://localhost:3500';
 export const axiosI = axios.create({
   baseURL: baseURL,
-  proxy: { host: 'nestjs', port: 3500 },
+  proxy: { host: 'fts.homelinux.com', port: 3500 },
 });
